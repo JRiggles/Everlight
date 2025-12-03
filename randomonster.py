825db51c7c5db2c95e6311171bab9f232b1c4837
John Riggles <jriggles@icloud.com>
2025-12-02 21:21:16 -0500

    refactor: replace "requests" with "httpx" for async

--- randomonster.py
+++ randomonster.py
@@ -1,27 +1,42 @@
-from requests import request
+import asyncio
+import httpx
 
 
-# request is completed at time of import when app server starts
 _base_url = 'https://www.dnd5eapi.co/api/2014/'
-_endpoints = ('magic-items', 'monsters', 'spells')
 _headers = {'Accept': 'application/json'}
+_endpoints = ('magic-items', 'monsters', 'spells')
 
-# track used names so random names can be recycled as presets are deleted
-used_names = set()
-names = set()
-# append item names to the "names" set for each of the 5e API endpoints
-for _url in map(_base_url.__add__, _endpoints):
-    _response = request('GET', _url, headers=_headers)
-    names = names.union(
-        {item['name'] for item in _response.json().get('results', [])}
-    )
+
+async def _fetch_names() -> set[str]:
+    """Fetch item names from the 5e API endpoints asynchronously."""
+    _names = set()
+    async with httpx.AsyncClient(
+        base_url=_base_url, headers=_headers
+    ) as client:
+        request_tasks = [client.get(endpoint) for endpoint in _endpoints]
+        for response in await asyncio.gather(*request_tasks):
+            if response.status_code == 200:
+                _names = _names.union(
+                    {i['name'] for i in response.json().get('results', [])}
+                )
+    return _names
 
 
 def get_dnd() -> str | None:
-    """Return a random dnd item, monster, or spell name"""
+    """Return a random dnd item, monster, or spell name."""
     try:
-       choice = names.pop()
-       used_names.add(choice)
-       return choice
-    except KeyError, IndexError:  # set empty
+        choice = names.pop()
+        used_names.add(choice)
+        return choice
+    except KeyError:  # set empty
         return None
+
+
+# run fetch_names coroutine to populate set of names
+names = asyncio.run(_fetch_names())
+# track used names so random names can be recycled as presets are deleted
+used_names = set()
+
+
+if __name__ == '__main__':
+    print(names)
