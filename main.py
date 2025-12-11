a8b16895e8add0ad968f34bc2dfcfebfbb26a680
John Riggles <jriggles@icloud.com>
2025-12-11 06:41:19 -0500

    add reset on quit, preset export. UI cleanup. Switch to "native" mode for built app

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+import shutil
+
+from contextlib import suppress
 from dataclasses import dataclass
+from multiprocessing import freeze_support  # noqa
+from pathlib import Path
 from typing import Optional
 from uuid import uuid4
 
-from nicegui import app, ui, Client
+from nicegui import app, native, ui
 from nicegui.observables import ObservableDict
+
 from lights import LightController
 from randomonster import get_dnd, names, used_names
+
+# TODO: better fonts for title, UI
+# TODO: fix help modal position in non-fullscreen viewports
+# TODO: randomizer for colors
+# TODO: easier editing of preset names
+# TODO: import presets (storage-general.json)
+# TODO: add README, publish to GitHub
+# TODO: preconfigured animations
+# TODO: drag to rearrange presets
+# TODO: add scene transition timeline? Might be verrrry complicated...
+# TODO: custom animations (needs scene timeline probably)
+# TODO: replace lambdas in colorpicker callbacks with proper functions
+# TODO: figure out a better way to have useful functions in '/' besides using
+#       a ton of inner functions
+
+VERSION = '1.1.0'
 
 
 @dataclass
@@ -17,19 +39,46 @@ class LightBoardPreset:
     brightness2: int
     name: Optional[str] = ''
 
-
-lc = LightController('10.0.42.2')
-# store last selected colors for each light
-last_color = dict.fromkeys([lc.D1.name, lc.D2.name], '')  # type: ignore
-
 # UI color scheme
 drow = '#6b6b88'
 orc = '#91a32b'
-dragon = "#BE3D20"
+dragon = '#BE3D20'
+behir = "#5680AD"
+
+with suppress(ConnectionRefusedError):
+    # placeholder for LightController instance
+    lc = LightController('127.0.0.1')
+# FIXME: this is gross, but I can't think of a better way to give the
+# on_shutdown handler access to the lc - it's early and I'm tired
+
+
+@app.on_shutdown
+def cleanup() -> None:
+    if lc is not None and isinstance(lc, LightController):
+        lc.reset_lights()
+
+
+@app.on_page_exception
+def connection_error_page(exception: Exception) -> None:
+    if not isinstance(exception, ConnectionError):
+        raise
+    with ui.column().classes('absolute-center items-center gap-8'):
+        ui.icon('wifi_off', size='xl')
+        ui.label(f'{exception}').classes('text-2xl')
 
 
 @ui.page('/')
-def index(client: Client) -> None:
+def index() -> None:
+    global lc
+    try:
+        lc = LightController('10.0.42.2')
+    except OSError:  # failed to connect to bridge
+        # NOTE: rasing an exception will put up the NiceGUI error page
+        raise ConnectionError('Check your internet connection and try again')
+
+    # store last selected colors for each light
+    last_color = dict.fromkeys([lc.D1.name, lc.D2.name], '')  # type: ignore
+
     app.add_static_files('/static', 'static')
     ui.query('body').style(
         "background-image: url('static/bg1.jpeg');"
@@ -40,16 +89,32 @@ def index(client: Client) -> None:
         "backdrop-filter: blur(0.33rem) brightness(42%);"
     )
     # set color scheme
-    ui.colors(primary=drow, positive=orc, negative=dragon)
-    ui.markdown("## 💡 LightBoard")
+    ui.colors(
+        primary=drow,
+        positive=orc,
+        negative=dragon,
+        info=behir,
+    )
 
     # help button
-    with ui.row().classes('absolute top-0 right-0 p-4'):
-        ui.button(icon='help', on_click=lambda: help_modal.open())
+    with ui.row().classes('items-baseline'):
+        # heading
+        ui.markdown("### Everlight").classes('ml-4')
+        ui.button(
+            icon='help',
+            on_click=lambda: help_modal.open()
+        ).classes('absolute top-4 right-4 px-4 py-2')
 
     # help modal
     with ui.dialog() as help_modal, ui.card().classes('no-shadow'):
-        ui.markdown("### LightBoard Help")
+        with ui.row().classes('items-baseline'):
+            ui.markdown('### Everlight Help')
+            ui.markdown(f'Version {VERSION}').classes(
+                'text-sm text-gray-500 mr-auto'
+            )
+            ui.image('static/appicon.png').classes(
+                'w-16 h-16 absolute right-4'
+            )
         ui.separator()
         ui.markdown(
             """
@@ -68,6 +133,13 @@ def index(client: Client) -> None:
         )
         ui.button('Okay', on_click=lambda: help_modal.close())
 
+    def reset_lights() -> None:
+        lc.reset_lights()
+        # restore the picker buttons to the default pirmary color
+        picker_1.style('background-color: var(--q-primary) !important;')
+        picker_2.style('background-color: var(--q-primary) !important;')
+        ui.notify('Lights reset', type='info')
+
     def save_preset() -> None:
         """Save the current light settings as a preset in user storage"""
         preset = LightBoardPreset(
@@ -81,22 +153,25 @@ def index(client: Client) -> None:
         )
         if preset_name.value:
             preset_name.value = ''  # clear preset name input on save
-        app.storage.user[preset.name] = preset  # store preset
-        ui.notify('Preset saved!', color='positive')
+        app.storage.general[preset.name] = preset  # store preset
+        ui.notify('Preset saved!', type='positive')
         show_presets()
 
     def show_presets() -> None:
         """Display saved presets from user storage"""
-        if app.storage.user:  # show presets card if any presets are saved
+        if app.storage.general:  # show presets card if any presets are saved
+            print(app.storage.general)  # DEBUG
             presets_card.visible = True
         else:  # otherwise hide the card
             presets_card.visible = False
             return
         preset_container.clear()
-        for name, preset in app.storage.user.items():
+        for name, preset in app.storage.general.items():
             if isinstance(preset, ObservableDict):
                 # convert ObservableDict back to LightBoardPreset dataclass
                 preset = LightBoardPreset(**preset)
+            elif isinstance(preset, str):
+                return
             with preset_container, ui.card().props('flat bordered'):
                 ui.markdown(f'##### {preset.name}')
                 with ui.row():
@@ -108,7 +183,9 @@ def index(client: Client) -> None:
                 with ui.row().classes('justify-between'):
                     ui.button(
                         'Apply',
-                        on_click=lambda p=preset: apply_preset(p)
+                        on_click=(
+                            lambda p=preset: apply_preset(p)  # type: ignore
+                        )
                     )
                     ui.button(
                         'Delete',
@@ -133,17 +210,48 @@ def index(client: Client) -> None:
         picker_2.update()
         picker_1.update()
         # notify user
-        ui.notify('Preset applied', color='primary')
+        ui.notify('Preset applied', type='info', color='primary')
 
+    # REFACTOR: prompt for confirmation before deleting
+    # see https://nicegui.io/documentation/dialog#awaitable_dialog for example
     def delete_preset(name: str) -> None:
         """Delete the preset with the given name from storage"""
-        if name in app.storage.user:
-            del app.storage.user[name]
+        if name in app.storage.general:
+            del app.storage.general[name]
             if name in used_names:
                 used_names.remove(name)
                 names.add(name)
-            ui.notify(f'Preset "{name}" deleted', color='negative')
+            ui.notify( f'Preset "{name}" deleted', color='negative')
             show_presets()
+
+    def export_presets() -> None:
+        """Export presets to the Downloads folder"""
+        # resolve path inside the app bundle
+        basepath = Path(__file__).resolve().parent
+        storage_path = basepath / '.nicegui' / 'storage-general.json'
+
+        if not storage_path.exists():
+            ui.notify(
+                f'Storage file not found at: {storage_path}',
+                type='negative'
+            )
+            return
+
+        downloads_folder = Path.home() / 'Downloads'
+        shutil.copy(storage_path, downloads_folder)
+        destination = downloads_folder / 'Everlight Presets.json'
+        shutil.move(downloads_folder / 'storage-general.json', destination)
+
+        if destination.exists():
+            ui.notify('Presets exported to Downloads folder!', type='positive')
+        else:
+            ui.notify('Exporting presets failed', type='negative')
+
+    def import_presets() -> None:  # TODO: implement importing
+        """Import a presets file"""
+        # resolve path inside the app bundle
+        basepath = Path(__file__).resolve().parent
+        storage_path = basepath / '.nicegui' / 'storage-general.json'
 
     # main UI layout
     # REFACTOR: the light controls are essentially identical for both lights;
@@ -173,7 +281,9 @@ def index(client: Client) -> None:
                         picker_1.style(
                             f'background-color: {e.color} !important;'
                         ),
-                        last_color.update({lc.D1.name: e.color}),
+                        last_color.update(
+                            {lc.D1.name: e.color}  # type: ignore
+                        ),
                     ]
                 )
             # show minimal color picker
@@ -208,7 +318,9 @@ def index(client: Client) -> None:
                         picker_2.style(
                             f'background-color: {e.color} !important;'
                         ),
-                        last_color.update({lc.D2.name: e.color}),
+                        last_color.update(
+                            {lc.D2.name: e.color}  # type: ignore
+                        ),
                     ]
                 )
                 # show minimal color picker
@@ -224,7 +336,7 @@ def index(client: Client) -> None:
 
     # global controls
     with ui.row():
-        ui.button('Reset', on_click=lc.reset_lights)
+        ui.button('Reset', on_click=reset_lights)
         # FIXME: I'm almost 100% sure setattr isn't the right way to do this
         ui.button('All On', on_click=(
             lambda _: [
@@ -238,7 +350,13 @@ def index(client: Client) -> None:
                 setattr(on_off_2, 'value', False)
             ]
         ))
+        ui.separator().props('vertical')
         ui.button('Save as Preset', on_click=save_preset)
+        ui.button('Export Presets', on_click=export_presets)
+        ui.button(
+            'Import Presets',
+            on_click=import_presets
+        ).props('disabled').tooltip('Coming soon!')
     with ui.row().classes('w-1/4'):
         preset_name = ui.input(
             'Preset Name (optional):',
@@ -251,12 +369,17 @@ def index(client: Client) -> None:
     show_presets()
 
 
-if __name__ in {"__main__", "__mp_main__"}:
-    # app.native.window_args['resizable'] = True
+if __name__ in {'__main__', '__mp_main__'}:
+    freeze_support()  # noqa
+    app.native.window_args['resizable'] = True
+    # app.native.start_args['debug'] = True
     ui.run(
-        title='LightBoard',
-        reload=True,
+        # reload=True,
+        title='Everlight - D&D Lightboard',
+        fullscreen=True,
         dark=True,
-        storage_secret='lightboard_secret',
-        # native=True,
+        # storage_secret='lightboard_secret',
+        native=True,
+        window_size=(1000, 625),
+        port=native.find_open_port()
     )
