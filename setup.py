7cdbafc3ce72cad0a159c7e5ef5fde938f0094dd
John Riggles <jriggles@icloud.com>
2025-12-11 06:36:36 -0500

    add setup.py for building with py2app
    nicegui-pack was having issues...

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from main import VERSION
+
+# NOTE: using py2app because nicegui-pack (based on pyinstaller) isn't working
+# - the built app crashes immediately after opening
+
+APP = ['main.py']
+DATA_FILES = [('static', ['static/bg1.jpeg']), '.nicegui']
+OPTIONS = {
+    'argv_emulation': False,
+    'iconfile': 'static/appicon.png',
+    'plist': {
+        'CFBundleIdentifier': 'com.jriggles.everlight',
+        'CFBundleShortVersionString': VERSION,
+        'LSUIElement': False,  # not a menu bar app
+        'NSHumanReadableCopyright': (
+            'Copyright © 2025 John Riggles [sudo_whoami] - MIT License'
+        ),
+    },
+    'packages': ['anyio', 'httpx', 'nicegui', 'phue', 'pygments', 'uvicorn',],
+}
+
+setup(
+    app=APP,
+    name='Everlight',
+    version=VERSION,
+    description='Lighting controller for our at-home D&D game',
+    author='J. Riggles [sudo_whoami]',
+    url='https://github.com/JRiggles/Everlight',
+    license='MIT',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
